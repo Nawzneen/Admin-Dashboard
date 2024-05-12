af594dea0308849e81008d102dec22d0cac3b172
Nazanin <earworm90@gmail.com>
2024-05-12 20:16:04 +0300

    Feat: added a feature to create a new genre

--- src/pages/genres/index.tsx
+++ src/pages/genres/index.tsx
@@ -1,4 +1,18 @@
-import { Heading } from "@aws-amplify/ui-react";
+import { Heading, Flex, Button } from "@aws-amplify/ui-react";
+import { useRouter } from "next/router";
 export default function Genre() {
-  return <Heading level={1}>This is genres</Heading>;
+  const router = useRouter();
+  return (
+    <Flex
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      className="underline"
+    >
+      <Heading level={1}>Genres</Heading>
+      <Button variation="primary" onClick={() => router.push("/genres/new")}>
+        Create Genre
+      </Button>
+    </Flex>
+  );
 }
--- src/pages/genres/new.tsx
+++ src/pages/genres/new.tsx
@@ -1,4 +1,18 @@
-import { Heading } from "@aws-amplify/ui-react";
+import GenreCreateForm from "@/ui-components/GenreCreateForm";
+import { Heading, Flex } from "@aws-amplify/ui-react";
+import router from "next/router";
 export default function GenreNew() {
-  return <Heading level={1}>This is a new Genre</Heading>;
+  return (
+    <>
+      <Heading level={1}>Create New Genre</Heading>
+      <Flex justifyContent="center">
+        <GenreCreateForm
+          width="340px"
+          border="1px solid black"
+          borderRadius="1 rem"
+          onSuccess={() => router.push("/genres")}
+        ></GenreCreateForm>
+      </Flex>
+    </>
+  );
 }
