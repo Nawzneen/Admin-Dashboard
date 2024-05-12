3e775f19f933fec3fd5553946a90b79cb0a980d3
Nazanin <earworm90@gmail.com>
2024-05-12 19:26:12 +0300

    init: create an app using amplify and next.js

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "AdminDashboard": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "AdminDashboard": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3admindashboardstorage50fa1521": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
