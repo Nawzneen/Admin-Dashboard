739adaddc9b0c5380ceb582621c6a3b39bb07543
Nazanin <earworm90@gmail.com>
2024-05-12 20:42:19 +0300

    Feat: add queries to the genres

--- src/pages/genres/index.tsx
+++ src/pages/genres/index.tsx
@@ -1,7 +1,25 @@
 import { Heading, Flex, Button } from "@aws-amplify/ui-react";
+import { TableValues } from "@/types/types";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import * as queries from "@/graphql/queries";
+import { GraphQLQuery } from "@aws-amplify/api";
+import { ListGenresQuery } from "@/API";
+// import { API } from "aws-amplify"; / No longer supported
+import { generateClient } from "aws-amplify/api";
 export default function Genre() {
+  const client = generateClient();
   const router = useRouter();
+  const [genre, setGenre] = React.useState<TableValues[]>();
+  useEffect(() => {
+    async function grabGenres() {
+      const allGenres = await client.graphql<GraphQLQuery<ListGenresQuery>>({
+        query: queries.listGenres,
+      });
+      setGenre(allGenres.data?.listGenres?.items as TableValues[]);
+    }
+    grabGenres();
+  }, []);
   return (
     <Flex
       direction="row"
