ad1192e33ddd1014ae9163a1d4cde19884afe9cf
Nazanin <earworm90@gmail.com>
2024-05-13 18:45:55 +0300

    Feat: add tables for products

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,4 +1,54 @@
-import { Heading } from "@aws-amplify/ui-react";
-export default function Product() {
-  return <Heading level={1}>This is Products</Heading>;
+import { Heading, Flex, Button } from "@aws-amplify/ui-react";
+import { TableValues } from "@/types/types";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as queries from "@/graphql/queries";
+import { GraphQLQuery } from "@aws-amplify/api";
+import { ListProductsQuery } from "@/API";
+// import { API } from "aws-amplify"; / No longer supported
+import { generateClient } from "aws-amplify/api";
+import ItemsTable from "@/components/items-table";
+export default function Genre() {
+  const client = generateClient();
+  const router = useRouter();
+  const [products, setProducts] = React.useState<TableValues[]>();
+  useEffect(() => {
+    async function grabProducts() {
+      const allProdcuts = await client.graphql<GraphQLQuery<ListProductsQuery>>(
+        {
+          query: queries.listGenres,
+        }
+      );
+      setProducts(allProdcuts.data?.listProducts?.items as TableValues[]);
+    }
+    grabProducts();
+  }, []);
+  return (
+    <>
+      <Flex
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        className=""
+      >
+        <Heading level={1}>Products</Heading>
+        <Button
+          variation="primary"
+          onClick={() => router.push("/products/new")}
+        >
+          Create Product
+        </Button>
+      </Flex>
+      <Flex
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        className=" mt-4"
+      >
+        {products === undefined ? null : (
+          <ItemsTable tableName="Products" data={products}></ItemsTable>
+        )}
+      </Flex>
+    </>
+  );
 }
