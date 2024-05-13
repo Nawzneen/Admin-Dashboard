9c3288b996d44f9e4c17055e9e85320a03d59ecd
Nazanin <earworm90@gmail.com>
2024-05-13 18:46:24 +0300

    Feat: add create new platforms form

--- src/pages/platforms/new.tsx
+++ src/pages/platforms/new.tsx
@@ -1,4 +1,19 @@
-import { Heading } from "@aws-amplify/ui-react";
+import { Heading, Flex } from "@aws-amplify/ui-react";
+import PlatformCreateForm from "@/ui-components/PlatformCreateForm";
+import router from "next/router";
+
 export default function PlatformNew() {
-  return <Heading level={1}>This is a new Platform</Heading>;
+  return (
+    <>
+      <Heading level={1}>Create New Platform</Heading>
+      <Flex justifyContent="center">
+        <PlatformCreateForm
+          width="340px"
+          border="1px solid black"
+          borderRadius="1 rem"
+          onSuccess={() => router.push("/platforms")}
+        ></PlatformCreateForm>
+      </Flex>
+    </>
+  );
 }
