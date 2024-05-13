cbe13b2cf3efcbe64ddc51bd487ba0831f645cd9
Nazanin <earworm90@gmail.com>
2024-05-13 18:47:52 +0300

    Feat: add platforms table

--- src/pages/platforms/index.tsx
+++ src/pages/platforms/index.tsx
@@ -1,4 +1,54 @@
-import { Heading } from "@aws-amplify/ui-react";
+import { Heading, Flex, Button } from "@aws-amplify/ui-react";
+import { useRouter } from "next/router";
+import { TableValues } from "@/types/types";
+import React, { useEffect } from "react";
+import * as queries from "@/graphql/queries";
+import { GraphQLQuery } from "@aws-amplify/api";
+import { ListPlatformsQuery } from "@/API";
+import { generateClient } from "aws-amplify/api";
+import ItemsTable from "@/components/items-table";
 export default function Platform() {
-  return <Heading level={1}>This is a Platforms</Heading>;
+  const client = generateClient();
+  const router = useRouter();
+  const [platforms, setPlatforms] = React.useState<TableValues[]>();
+  useEffect(() => {
+    async function grabPlatforms() {
+      const allPlatforms = await client.graphql<
+        GraphQLQuery<ListPlatformsQuery>
+      >({
+        query: queries.listPlatforms,
+      });
+      setPlatforms(allPlatforms.data?.listPlatforms?.items as TableValues[]);
+    }
+    grabPlatforms();
+  }, []);
+  console.log(platforms);
+  return (
+    <>
+      <Flex
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        className=""
+      >
+        <Heading level={1}>Platforms</Heading>
+        <Button
+          variation="primary"
+          onClick={() => router.push("/platforms/new")}
+        >
+          Create Platform
+        </Button>
+      </Flex>
+      <Flex
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        className=" mt-4"
+      >
+        {platforms === undefined ? null : (
+          <ItemsTable tableName="Platforms" data={platforms}></ItemsTable>
+        )}
+      </Flex>
+    </>
+  );
 }
