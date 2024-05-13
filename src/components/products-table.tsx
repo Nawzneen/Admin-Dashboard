8477f92c53ecd88766d6c66e3837815b56609da6
Nazanin <earworm90@gmail.com>
2024-05-13 18:59:34 +0300

    Feat: complete products table

--- src/components/products-table.tsx
+++ src/components/products-table.tsx
@@ -7,3 +7,47 @@ import {
   TableBody,
 } from "@aws-amplify/ui-react";
 import { StorageImage } from "@aws-amplify/ui-react-storage";
+interface ProductsTableProps {
+  products: Product[];
+}
+export default function ProductsTable({ products }: ProductsTableProps) {
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Image</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Price</TableCell>
+          <TableCell>Genre</TableCell>
+          <TableCell>Platform</TableCell>
+          <TableCell>Date Added</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {products.map((product, key) => (
+          <TableRow key={key}>
+            <TableCell>
+              {product.image === null || product?.image === undefined ? null : (
+                <StorageImage
+                  width={"250px"}
+                  accessLevel="private"
+                  alt="image"
+                  imgKey={product?.image}
+                />
+              )}
+            </TableCell>
+            <TableCell>{product?.name}</TableCell>
+            <TableCell>{product?.price}</TableCell>
+            <TableCell>{product?.Genre?.name}</TableCell>
+            <TableCell>{product?.Platform?.name}</TableCell>
+            <TableCell>
+              {Intl.DateTimeFormat("en-us").format(
+                new Date(product?.createdAt!)
+              )}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
