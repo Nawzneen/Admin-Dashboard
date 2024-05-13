de24a941cfedee36d7ed9cdf8a2c5ac534261200
Nazanin <earworm90@gmail.com>
2024-05-13 18:45:36 +0300

    Feat: Add create new products forms

--- src/pages/products/new.tsx
+++ src/pages/products/new.tsx
@@ -1,4 +1,21 @@
-import { Heading } from "@aws-amplify/ui-react";
+import { Heading, Flex } from "@aws-amplify/ui-react";
+import ProductCreateForm from "@/ui-components/ProductCreateForm";
+import router from "next/router";
+
 export default function ProductNew() {
-  return <Heading level={1}>This is a new Product</Heading>;
+  return (
+    <>
+      <Flex justifyContent="center" alignItems="center" direction="column">
+        <Heading level={1} className="my-10">
+          Create New Product
+        </Heading>
+        <ProductCreateForm
+          width="340px"
+          border="1px solid black"
+          borderRadius="1 rem"
+          onSuccess={() => router.push("/products")}
+        ></ProductCreateForm>
+      </Flex>
+    </>
+  );
 }
