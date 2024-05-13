2852520f5e838f5363ff79f1a10767e73d6fdc6d
Nazanin <earworm90@gmail.com>
2024-05-13 18:52:19 +0300

    Feat: create products table

---
+++ src/components/products-table.tsx
@@ -0,0 +1,9 @@
+import { GetGenresQueryVariables, Product } from "@/API";
+import {
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@aws-amplify/ui-react";
+import { StorageImage } from "@aws-amplify/ui-react-storage";
