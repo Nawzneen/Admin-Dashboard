d279a43f4311e98d83a3cb44b671c6a05e5db7f0
Nazanin <earworm90@gmail.com>
2024-05-13 19:22:12 +0300

    Feat: add products to the first page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Heading, Button, Flex, useAuthenticator } from "@aws-amplify/ui-react";
+import React, { useEffect } from "react";
+import { Product } from "@/API";
+import { GraphQLQuery, generateClient } from "aws-amplify/api";
+import * as queries from "@/graphql/queries";
+import { ListProductsQuery } from "@/API";
+import ProductsTable from "@/components/products-table";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const client = generateClient();
   const { user, signOut } = useAuthenticator((context) => [context.user]);
+  const [products, setProducts] = React.useState<Product[]>();
+  useEffect(() => {
+    async function grabProducts() {
+      const allProducts = await client.graphql<GraphQLQuery<ListProductsQuery>>(
+        {
+          query: queries.listProducts,
+        }
+      );
+
+      setProducts(allProducts.data?.listProducts?.items as Product[]);
+    }
+    grabProducts();
+  }, []);
   return (
     <>
       <Flex
@@ -13,11 +33,15 @@ export default function Home() {
         alignItems={"center"}
         direction={"column"}
       >
-        <Heading level={1}>Hello World</Heading>
+        {/* <Heading level={1}>Hello World</Heading> */}
         <Button onClick={signOut} variation="primary">
           Sign Out
         </Button>
+        <Heading level={2}>All Products</Heading>
       </Flex>
+      {products === undefined ? null : (
+        <ProductsTable data={products}></ProductsTable>
+      )}
     </>
   );
 }
